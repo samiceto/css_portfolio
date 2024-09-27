d471af6b7f5366e77b23c538dff8042ef3fd7b7a
Sami Ullah <samiullahmalik7474@gmail.com>
2024-09-27 17:57:44 +0500

    .2

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -88,7 +88,7 @@ export const BentoGridItem = ({
             </div>
             {id === 6 && (
                 <BackgroundGradientAnimation>
-                    {/* <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div> */}
+                    <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
                 </BackgroundGradientAnimation>
             )}
             <div className={cn(
